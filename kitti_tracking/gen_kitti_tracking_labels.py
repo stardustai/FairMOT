090ea49af0ef2dcb642fbefd325735837a77cef3
Derek Zen <leizhang0121@gmail.com>
2021-06-30 14:07:47 +0800

    improved kitti convert script

--- kitti_tracking/gen_kitti_tracking_labels.py
+++ kitti_tracking/gen_kitti_tracking_labels.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from glob import glob
-import os, random, time
+import os, random, shutil
 from PIL import Image, ImageDraw
 from tqdm import tqdm
 
@@ -61,28 +61,29 @@ target_img_path = data_root+'/images'
 target_txt_path = data_root+'/labels_with_ids'
 ori_label_path = data_root+'/original_label/label_02'
 class_ID = {
-    'Car': 1,
-    'Van': 2,
-    'Truck': 3,
-    'Pedestrian': 4,
-    'Person_sitting': 5,
-    'Cyclist': 6, 
-    'Tram': 7,
-    'Misc': 8,
-    'DontCare': 9,
-    'Person': 4
+    'Car': 0,
+    'Van': 1,
+    'Truck': 2,
+    'Pedestrian': 3,
+    'Person_sitting': 3,
+    'Cyclist': 3, 
+    'Tram': 4,
+    'Misc': 5,
+    # 'DontCare': 6,
+    'Person': 3
 }
+ID_class = {v:k for k, v in class_ID.items()}
 
-# def get_class_ID(name):
-#     if name in class_ID:
-#         return class_ID[name]
-#     else:
-#         max_id = max(class_ID.values())
-#         class_ID[name] = max_id + 1
-#         print(f'added class `{name}` to dict with value {max_id+1}')
-#         return class_ID[name]
+def get_class_ID(name):
+    if name in class_ID:
+        return class_ID[name]
+    else:
+        print(f'ignored class `{name}`')
+        return None
 
 def convert_kitti():
+    # shutil.rmtree(target_img_path)
+    shutil.rmtree(target_txt_path)
     tasks = glob(ori_img_path+'/*')
     tasks = [t.split('/')[-1] for t in tasks]
     print(f'Found images for task: {tasks}')
@@ -104,23 +105,25 @@ def convert_kitti():
         label_df['y_center'] = (label_df.top + label_df.bottom)/2/h
         label_df['w_p'] = (label_df.right - label_df.left)/w
         label_df['h_p'] = (label_df.bottom - label_df.top)/h
-        label_df['class'] = label_df.type.apply(lambda x: class_ID[x])
-        frames = set(label_df.frame.to_list())
-        for f in tqdm(frames):
+        label_df['class'] = label_df.type.apply(lambda x: get_class_ID(x))
+        # drop none class
+        label_df2 = label_df.dropna(how='any')
+        for f in tqdm(range(label_df.frame.max()+1)):
             txt_path = f'{target_txt_path}/{task}/{f:06}.txt'
             img_path = f'{target_img_path}/{task}/{f:06}.jpg'
             if not os.path.exists(img_path):
                 img = Image.open(f'{ori_img_path}/{task}/{f:06}.png')
                 assert w == img.width and h == img.height
                 img.save(img_path)
-            if not os.path.exists(txt_path):
-                txt_df = label_df.query('frame == @f')[['class', 'track_id', 'x_center', 'y_center', 'w_p', 'h_p']]
-                txt_df.to_csv(txt_path, header=False, index=False, sep=' ')
+            # if not os.path.exists(txt_path):
+            txt_df = label_df2.query('frame == @f')[['class', 'track_id', 'x_center', 'y_center', 'w_p', 'h_p']]
+            txt_df.to_csv(txt_path, header=False, index=False, sep=' ')
 
 
 # validate generated annotation file
-def render_img(img_path, txt):
+def render_img(img_path, save_path=None):
     img = Image.open(img_path)
+    txt = img_path.replace('images', 'labels_with_ids').replace('.jpg', '.txt')
     w, h = img.width, img.height
     draw = ImageDraw.Draw(img, 'RGBA')
     anno = pd.read_csv(txt, names=['class', 'track_id', 'x_center', 'y_center', 'w_p', 'h_p'], sep=' ')
@@ -130,16 +133,21 @@ def render_img(img_path, txt):
     anno['y2'] = (anno.y_center + anno.h_p/2) * h
     for i, row in anno.iterrows():
         draw.rectangle([(row.x1, row.y1), (row.x2, row.y2)])
-    img.show(title='anno')
+        draw.text((row.x1, row.y1), f'{ID_class[row["class"]]}({row.track_id})')
+    if not save_path:
+        img.show(title='anno')
+    else:
+        os.makedirs('/'.join(save_path.split('/')[:-1]), exist_ok=True)
+        img.save(save_path)
 
 
-def show_rand_img():
-    imgs = glob(target_img_path+'/**/*.jpg', recursive=True)
-    img = random.choice(imgs)
+def show_img(img=None):
+    if not img:
+        imgs = glob(target_img_path+'/**/*.jpg', recursive=True)
+        img = random.choice(imgs)
     frame = img.split('/')[-1].split('.')[0]
     print(f'ploting task: {frame}')
-    txt = img.replace('images', 'labels_with_ids').replace('.jpg', '.txt')
-    render_img(img, txt)
+    render_img(img)
 
 def gen_train_val():
     tasks = glob(target_img_path+'/*')
@@ -160,8 +168,23 @@ def gen_train_val():
             for t in images:
                 f.write(t+'\n')
 
+def render_video():
+    rander_path = 'kitti_tracking/rendered'
+    tasks = glob(target_img_path + '/*')
+    for task in tasks:
+        seq = task.split('/')[-1]
+        imgs = glob(f'{target_img_path}/{seq}/*.jpg')
+        for img in tqdm(imgs):
+            name = img.split('/')[-1]
+            render_img(img, f'{rander_path}/{seq}/{name}')
+        output_video_path = f'{rander_path}/{seq}.mp4'
+        cmd_str = f'ffmpeg -y -framerate 10 -f image2 -i {rander_path}/{seq}/%06d.jpg -c:v libx264 -preset fast -x264-params crf=25 -vf "crop=trunc(iw/2)*2:trunc(ih/2)*2" -r 30 {output_video_path}'
+        print(f'>>>Running ffmpeg with cmd: {cmd_str}')
+        os.system(cmd_str)
 
 if __name__ == '__main__':
-    # convert_kitti()
-    # gen_train_val()
-    show_rand_img()
+    convert_kitti()
+    gen_train_val()
+    show_img()
+    render_video()
+    
